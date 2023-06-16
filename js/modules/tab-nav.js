b3e42aba9c1d957b54785298a2e56e1c46486d71
Lucas França <lucas.fnev@gmail.com>
2023-06-16 14:03:24 -0300

    fixing errors in tabnav

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,25 +1,28 @@
 export default class TabNav {
   constructor(menu, content) {
-    this.tabMenu = menu;
-    this.tabContent = content;
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
     this.activeClass = "active";
   }
   activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("active");
-    });
-    tabContent[index].classList.add("active", tabContent[index].dataset.anime);
+    this.tabContent.forEach((section) =>
+      section.classList.remove(this.activeClass)
+    );
+    this.tabContent[index].classList.add(
+      this.activeClass,
+      this.tabContent[index].dataset.anime
+    );
   }
 
   addTabNavEvent() {
     this.tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener("click", () => activeTab(index));
+      itemMenu.addEventListener("click", () => this.activeTab(index));
     });
   }
 
   init() {
     if (this.tabMenu.length && this.tabContent.length) {
-      this.activeClass(0);
+      this.activeTab(0);
       this.addTabNavEvent();
     }
   }
--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ const tabnav = new TabNav(
 );
 tabnav.init();
 initAnimationScroll();
-initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
