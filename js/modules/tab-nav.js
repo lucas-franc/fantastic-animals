b731c3a61a5ab3bd433d9d20b68a50a9f0c6db99
Lucas França <lucas.fnev@gmail.com>
2023-06-21 11:51:21 -0300

    correcting errors

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -2,10 +2,6 @@ export default class TabNav {
   constructor(menu, content) {
     this.tabMenu = document.querySelectorAll(menu);
     this.tabContent = document.querySelectorAll(content);
-
-    this.tabMenu = document.querySelector(menu);
-    this.tabContent = document.querySelector(content);
-
     this.activeClass = "active";
   }
   activeTab(index) {
