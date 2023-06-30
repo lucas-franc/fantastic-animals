865bd0da7dc2e8cf30fd6196565670a344d12039
Lucas França <lucas.fnev@gmail.com>
2023-06-30 14:32:14 -0300

    dropdown menu function to class

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,18 +1,37 @@
-import outsideClick from "./outsideclick.js";
+import outsideClick from './outsideclick.js';
 
-export default function initDropdownMenu() {
-  function handleClick(event) {
+export default class DropdownMenu {
+  constructor(dropdown, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdown);
+    this.activeClass = 'active';
+
+    if (events == undefined) this.events = ['touchstart', 'click'];
+    else this.events = events;
+
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add("active");
-    outsideClick(this, ["touchstart", "click"], () => {
-      this.classList.remove("active");
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, ['touchstart', 'click'], () => {
+      element.classList.remove(this.activeClass);
     });
   }
-  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
-  dropdownMenus.forEach((menu) => {
-    menu.addEventListener("click", handleClick);
-    ["touchstart", "click"].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+
+  addDropdownMenuEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      menu.addEventListener('click', this.activeDropdownMenu);
+      ['touchstart', 'click'].forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
     });
-  });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownMenuEvent();
+    }
+    return this;
+  }
 }
