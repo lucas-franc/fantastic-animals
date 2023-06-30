1afc3992f518a7107b6cef6c626cac963c286488
Lucas França <lucas.fnev@gmail.com>
2023-06-30 11:27:00 -0300

    function to class

--- js/modules/animation-scroll.js
+++ js/modules/animation-scroll.js
@@ -1,20 +1,24 @@
-export default function initAnimationScroll() {
-  const sections = document.querySelectorAll("[data-anime='scroll']");
-  function scrollAnimation() {
-    const windowHalf = window.innerHeight * 0.6;
-    sections.forEach((section) => {
+export default class AnimationScroll {
+  constructor(anime) {
+    this.sections = document.querySelectorAll(anime);
+    this.windowHalf = window.innerHeight * 0.6;
+    this.scrollAnimation = this.scrollAnimation.bind(this);
+  }
+
+  scrollAnimation() {
+    this.sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - windowHalf < 0;
+      const isSectionVisible = sectionTop - this.windowHalf < 0;
       if (isSectionVisible) {
-        section.classList.add("active");
-      } else if (section.classList.contains("active")) {
-        section.classList.remove("active");
+        section.classList.add('active');
+      } else if (section.classList.contains('active')) {
+        section.classList.remove('active');
       }
     });
   }
-  if (sections.length) {
-    scrollAnimation();
 
-    window.addEventListener("scroll", scrollAnimation);
+  init() {
+    this.scrollAnimation();
+    window.addEventListener('scroll', this.scrollAnimation);
   }
 }
